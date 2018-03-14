2a9ca97bd5f5c3ebedbe12ee1251b8b1cd8eeaee
Giorgi Koguashvili <kogo27@gmail.com>
2018-03-14 10:57:50 +0400

    added monad implementation with bind method

--- Roslyner.Domain/Infrastructure/Either.cs
+++ Roslyner.Domain/Infrastructure/Either.cs
@@ -4,10 +4,15 @@ using System.Text;
 
 namespace Types.Union
 {
-    public class Either<TLeft, TRight> : Union<TLeft, TRight>
+    public class Either<TLeft, TRight> : Union<TLeft, TRight>,
+        IMonad<Either<TLeft, TRight>, TLeft, TRight>
            where TLeft : class
            where TRight : class
     {
+        public Either() : base((TRight)null)
+        {
+
+        }
         public Either(Func<Either<TLeft, TRight>> factory) : base(factory) { }
         public Either(TLeft t1) : base(t1)
         {
@@ -16,6 +21,39 @@ namespace Types.Union
         public Either(TRight t2) : base(t2)
         {
         }
+
+        public M1 Bind<M1, T2>(Func<TLeft, IMonad<M1, T2, TRight>> m) 
+            where M1 : IMonad<M1, T2, TRight>, new ()
+        {
+            var a = this
+                     .Match(
+                         (r) => m(r),
+                         (e) => new M1().Return(e)
+                     );
+            return (M1)a;
+            //throw new Exception();
+        }
+
+        public Either<TLeft, TRight> Return(TLeft t)
+        {
+            return new Either<TLeft, TRight>(t);
+        }
+
+        public Either<TLeft, TRight> Return(TRight t)
+        {
+            return new Either<TLeft, TRight>(t);
+        }
+    }
+
+
+    public interface IMonad<M0, T0, T1>
+        where M0 : IMonad<M0, T0, T1>, new ()
+    {
+        M0 Return(T0 t);
+        M0 Return(T1 t);
+
+        M1 Bind<M1, T2>(Func<T0, IMonad<M1, T2, T1>> m)
+            where M1 : IMonad<M1, T2, T1>, new ();
     }
 
     //public interface IKindTwo<T1, T2>
--- Roslyner.Web/Controllers/RoslynerController.cs
+++ Roslyner.Web/Controllers/RoslynerController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -9,14 +11,59 @@ using Roslyner.Domain;
 using Roslyner.Domain.ClassForInject;
 using Roslyner.Domain.Interfaces;
 using Roslyner.Web.Models;
+using Types.Union;
 
 namespace Roslyner.Web.Controllers
 {
+    public class InjectedClassResut : Either<Customer, CompileError>
+    {
+        public InjectedClassResut(IEnumerable<byte> compiledCode) : base(() =>
+        {
+            return new InjectedClassResut(
+                new InjectedClassCodeInstance<IRule>(
+                                        compiledCode,
+                                        new CodeTemplateForFooClass()
+                                    )
+                                    .Instance()
+                                    .Check());
+        }) { }
+        public InjectedClassResut(Customer t1) : base(t1)
+        {
+        }
+
+        public InjectedClassResut(CompileError t2) : base(t2)
+        {
+        }
+
+    }
+
+    public class Mattcher
+    {
+
+    }
     public class RoslynerController : Controller
     {
         [HttpPost]
         public JsonResult Build([FromBody] MonacoEditorModel model)
         {
+
+            return new CompiledCode(
+                        model.Code,
+                        new CodeTemplateForFooClass()
+                    )
+                    .Bind((a) => new InjectedClassResut(a))
+                    .Match(
+                        (compiledCode) => Json(
+                                                new BuildResult(
+                                                    JsonConvert
+                                                        .SerializeObject(
+                                                            compiledCode
+                                                        )
+                                                )
+                                            ),
+                        (error) => Json(new BuildResult(error.Message))
+                );
+
             return new CompiledCode(
                         model.Code,
                         new CodeTemplateForFooClass()
@@ -36,6 +83,25 @@ namespace Roslyner.Web.Controllers
                                         ),
                         (error) => Json(new BuildResult(error.Message))
                     );
+            //return new CompiledCode(
+            //            model.Code,
+            //            new CodeTemplateForFooClass()
+            //        ).Match(
+            //            (compiledCode) => Json(
+            //                                new BuildResult(
+            //                                    JsonConvert
+            //                                        .SerializeObject(
+            //                                            new InjectedClassCodeInstance<IRule>(
+            //                                                compiledCode,
+            //                                                new CodeTemplateForFooClass()
+            //                                            )
+            //                                            .Instance()
+            //                                            .Check()
+            //                                        )
+            //                                )
+            //                            ),
+            //            (error) => Json(new BuildResult(error.Message))
+            //        );
         }
     }
 }
